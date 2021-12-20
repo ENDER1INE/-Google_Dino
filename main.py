c916cbd7a805f785bb7c007b6b62bc7571a2dc9f
ENDER1INE <p2p.two@mail.ru>
2021-12-20 18:53:19 +0300

    Base

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+import sys
+
+
+SIZE = WIDTH, HEIGHT = 1800, 700
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+bg_w, bg_h = SIZE
+bg = pygame.transform.smoothscale(pygame.image.load('back.png'), (bg_w, bg_h))
+pos_x = 0
+speed = 10
+
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+running = True
+clock = pygame.time.Clock()
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    allKeys = pygame.key.get_pressed()
+    pos_x += speed if allKeys[pygame.K_LEFT] else -speed if allKeys[pygame.K_RIGHT] else 0
+
+    x_rel = pos_x % bg_w
+    x_part2 = x_rel - bg_w if x_rel > 0 else x_rel + bg_w
+
+    screen.blit(bg, (x_rel, 0))
+    screen.blit(bg, (x_part2, 0))
+    pygame.display.flip()
+pygame.quit()
