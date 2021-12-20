6fea3f56efe85988add1040ee6cf5ec7438d902a
ENDER1INE <p2p.two@mail.ru>
2021-12-20 18:58:01 +0300

    Base_0.1

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ SIZE = WIDTH, HEIGHT = 1800, 700
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -16,8 +15,8 @@ def load_image(name, colorkey=None):
     return image
 
 
-bg_w, bg_h = SIZE
-bg = pygame.transform.smoothscale(pygame.image.load('back.png'), (bg_w, bg_h))
+background_width, background_height = SIZE
+bg = pygame.transform.smoothscale(pygame.image.load('back.png'), (background_width, background_height))
 pos_x = 0
 speed = 10
 
@@ -34,8 +33,8 @@ while running:
     allKeys = pygame.key.get_pressed()
     pos_x += speed if allKeys[pygame.K_LEFT] else -speed if allKeys[pygame.K_RIGHT] else 0
 
-    x_rel = pos_x % bg_w
-    x_part2 = x_rel - bg_w if x_rel > 0 else x_rel + bg_w
+    x_rel = pos_x % background_width
+    x_part2 = x_rel - background_width if x_rel > 0 else x_rel + background_width
 
     screen.blit(bg, (x_rel, 0))
     screen.blit(bg, (x_part2, 0))
