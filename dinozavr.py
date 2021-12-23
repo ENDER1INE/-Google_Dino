fdb1dfaf1a2ca988279fc8f1eca9758a77954432
SofiaBerezina <95140523+SofiaBerezina@users.noreply.github.com>
2021-12-24 01:17:40 +0500

    Add files via upload

--- dinozavr.py
+++ dinozavr.py
@@ -4,7 +4,7 @@ import sys
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join(name)
+    fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -13,14 +13,30 @@ def load_image(name, colorkey=None):
 
 
 class Dino(pygame.sprite.Sprite):
-    dinosaur = load_image("images.jpeg")
+    dinosaur = load_image("dinoz.png")
+    dinosaur = pygame.transform.scale(dinosaur, (100, 100))
 
     def __init__(self, *group):
         super().__init__(*group)
-        self.dinosaur = Dino.dinosaur
+        self.image = Dino.dinosaur
         self.rect = self.dinosaur.get_rect()
-        self.rect.x = 400
-        self.rect.y = 500
+        x, y = 300, 250
+        self.rect.x = x
+        self.rect.y = y
+        self.isJump = False
+        self.jumpCount = 10
 
     def update(self):
-       pass
+        pass
+        # if self.isJump and self.rect.y == 0:
+        #     if self.jumpCount >= -10:
+        #         neg = 1
+        #         if self.jumpCount < 0:
+        #             neg = -1
+        #         self.rect.y -= self.jumpCount ** 2 * 0.1 * neg
+        #         self.jumpCount -= 1
+        #         self.isJump = False
+        #     else:
+        #         self.isJump = False
+        #         self.jumpCount = 10
+
