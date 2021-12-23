78c56898fde3c3d40a36fa692f625b1faf7856b0
ENDER1INE <p2p.two@mail.ru>
2021-12-23 22:50:34 +0300

    Base_0.4

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import pygame
 import os
 import sys
 
-
-SIZE = WIDTH, HEIGHT = 1800, 700
+SIZE = WIDTH, HEIGHT = 1000, 400
 
 
 def load_image(name, colorkey=None):
@@ -15,21 +14,24 @@ def load_image(name, colorkey=None):
     return image
 
 
-all_sprites = pygame.sprite.Group()
-Dino(all_sprites)
+def time_speed_up():  # <<< функция счета секунд
+    return int(pygame.time.get_ticks()) // 1000
 
+
+# <<< background
 background_width, background_height = SIZE
-background = pygame.transform.smoothscale(pygame.image.load('background.png'), (background_width, background_height))
+background = pygame.transform.smoothscale(load_image('background.png'), (background_width, background_height))
+# <<<
+
 
 pos_x = 0  # <<< Позиция старта
-speed = 15  # <<< Скорость движения
+speed = 3  # <<< Скорость движения
 
 start_flag = False  # <<< Флаг старта движения
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,17 +41,20 @@ while running:
     allKeys = pygame.key.get_pressed()
     if allKeys[pygame.K_SPACE]:
         start_flag = True
-
     if start_flag:
         pos_x -= speed
+    # >>>
+
+    #  Увеличение скорости каждые 2 сек
+    if time_speed_up() % 2 == 0:
+        speed += 0.001
     # >>>
 
     #  Движение заднего фона <<<
     coord_image_1 = pos_x % background_width
     coord_image_2 = coord_image_1 - background_width if coord_image_1 > 0 else coord_image_1 + background_width
-    screen.blit(background, (coord_image_1, 0))
-    screen.blit(background, (coord_image_2, 0))
-    pygame.display.flip()
-    all_sprites.draw(screen)
+    screen.blit(background, (coord_image_1, 0)), screen.blit(background, (coord_image_2, 0))
     # >>>
+    pygame.display.flip()
+
 pygame.quit()
